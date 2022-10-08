a8260ecfbaa1ec6fe5e3202739475526ea145bae
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-08 11:41:10 +0330

    should have been in another branch

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,8 +1,20 @@
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Loader, Error, RelatedSongs } from '../components';
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
 
 const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-  return <div>hey</div>
+  const { songid } = useParams();
+
+  return (
+    <div className="flex flex-col">
+      <DitailsHeader
+    </div>
+  )
 };
 
 export default SongDetails;
