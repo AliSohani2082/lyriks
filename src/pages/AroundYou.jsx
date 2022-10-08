71e999f5ddf939a4de5a0d61dd2574ad39d64e38
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-08 17:21:58 +0330

    added AroundYou page

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -25,7 +25,20 @@ const AroundYou = () => {
 
   return (
     <div className='flex flex-col'>
-      <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>around you</h2>
+      <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+        Around you <span className='font-black'>{country}</span>
+      </h2>
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+          />
+        ))}
+      </div>
     </div>
   )
 };
