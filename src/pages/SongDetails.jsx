a79a9181fc97f6a5b4ed8e4c47763a8f99bbc519
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-08 12:16:02 +0330

    added lyrics to teh songDetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -8,19 +8,27 @@ import { setActiveSong, playPause } from "../redux/features/playerSlice";
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-
   const { songid } = useParams();
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+
+
 
   return (
     <div className="flex flex-col">
-      {/* <DitailsHeader
-        artistId={artistId}
+      <DitailsHeader
+        //artistId={artistId}
         songData={songData}
-      /> */}
+      />
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics</h2>
         <div className="mt-5">
-          {}
+          {songData?.sections[1].type === 'LYRICS' ? 
+            songData?.sections[1].text.map((line, i) => (
+              <p className="text-gray-400 text-base my-1">{line}</p>
+            )) : (
+              <p className="text-gray-400 text-base my-1">Sorry, no lyrics found</p>
+            )
+          }
         </div>
       </div>
     </div>
