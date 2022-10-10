b73a3e85d0a91cf6ac94e20bb2afcf179a049482
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-10 12:26:01 +0330

    added nodemon

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const app = express()
 
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
-app.use(cors())
+app.use(cors());
 
 const PORT = 5000
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
+
