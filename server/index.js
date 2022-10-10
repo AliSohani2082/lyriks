b0d73d5da99f36541dc05a1fecd0d3c34967a1d5
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-10 12:02:02 +0330

    added back-end boiler plate

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(cors())
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
