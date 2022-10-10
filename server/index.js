97030e4813563ecca0b1ee4bcdfd7f0a87af3ad1
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-10-10 13:04:10 +0330

    added mongoose

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
 app.use(cors());
 
-const PORT = 5000
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000
 
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`)))
+  .catch((error) => console.log(error.message))
